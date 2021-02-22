a8056f4fc90d72ef2c0e080127d8db78b46c7936
unknown <pedrooarthur20@outlook.com>
2021-02-22 15:35:52 -0300

    jogo finalizado

--- script.js
+++ script.js
@@ -40,6 +40,15 @@ function update(event) {
 }
 
 function iniciarJogo() {
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+            
+        }
+    }
+
     if(snake[0].x > 15 * box && direction) snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -65,7 +74,7 @@ function iniciarJogo() {
     else{food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-    
+
     let newHead = {
         x: snakeX,
         y: snakeY
